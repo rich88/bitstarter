909c415ffc030b699cbeb9caa0a7321f28141d27
Rich Lin <rich88@gmail.com>
2013-07-09 16:28:55 +0000

    Define var buf

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buf = new Buffer;
   response.send(buf.toString(fs.readFileSync('index.html')));
 });
 
