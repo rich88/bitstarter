c0a4c8ea01737290c0e7bfacb1844a9f3639f351
Rich Lin <rich88@gmail.com>
2013-07-09 16:31:48 +0000

    Bug fix

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = new Buffer;
-  response.send(buf.toString(fs.readFileSync('index.html')));
+  var buf = new Buffer(fs.readFileSync('index.html'));
+  response.send(buf.toString);
 });
 
 var port = process.env.PORT || 5000;
